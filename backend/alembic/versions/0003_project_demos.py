27972341461475d3e16ed4569db13ddfea9e6ede
Joshue Torres <josuetorresf2@gmail.com>
2026-06-24 00:14:27 -0500

    Shorten Alembic revision id for PostgreSQL

--- backend/alembic/versions/0003_project_demos.py
+++ backend/alembic/versions/0003_project_demos.py
@@ -1,6 +1,6 @@
 """Add candidate project demo URLs.
 
-Revision ID: 0003_add_candidate_project_demo_urls
+Revision ID: 0003_project_demos
 Revises: 0002_add_user_phone_number
 Create Date: 2026-06-23
 """
@@ -8,7 +8,7 @@ Create Date: 2026-06-23
 from alembic import op
 import sqlalchemy as sa
 
-revision = "0003_add_candidate_project_demo_urls"
+revision = "0003_project_demos"
 down_revision = "0002_add_user_phone_number"
 branch_labels = None
 depends_on = None
